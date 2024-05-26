0ea7c7518a3a480a2a92bf90706e69244a00b91c
cheresna1991 <yaroslavcheg98@gmail.com>
2024-05-26 21:02:54 +0800

    доделана страница сереги

--- js/sergo.js
+++ js/sergo.js
@@ -9,3 +9,37 @@ a.addEventListener('click', () => {
         document.getElementById("aa").innerHTML = g;
     }
 });
+let b = document.getElementById('b');
+b.addEventListener('click', () => {
+    var ib = Number(document.querySelector('#ib>input').value);
+    if (ib === 11) {
+        var g = 'Правильно!';
+        document.getElementById("bb").innerHTML = g;
+    } else {
+        var g = 'Неправильно!';
+        document.getElementById("bb").innerHTML = g;
+    }
+});
+let c = document.getElementById('c');
+c.addEventListener('click', () => {
+    var ic = Number(document.querySelector('#ic>input').value);
+    if (ic === 54) {
+        var g = 'Правильно!';
+        document.getElementById("cc").innerHTML = g;
+    } else {
+        var g = 'Неправильно!';
+        document.getElementById("cc").innerHTML = g;
+    }
+});
+let d = document.getElementById('d');
+d.addEventListener('click', () => {
+    var id = Number(document.querySelector('#id>input').value);
+    if (id === 5050) {
+        var g = 'Правильно!';
+        document.getElementById("dd").innerHTML = g;
+    } else {
+        var g = 'Неправильно!';
+        document.getElementById("dd").innerHTML = g;
+    }
+});
+
