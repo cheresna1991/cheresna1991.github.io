4e6a76bf3bf03ee69e10c80bb69a919adad1583b
cheresna1991 <yaroslavcheg98@gmail.com>
2024-05-28 00:30:37 +0800

    то же самое

--- js/og.js
+++ js/og.js
@@ -1,8 +1,11 @@
 var object = document.getElementById('item');
-
+var a = document.getElementById('scream')
 object.onmouseenter=function(){
     var x = Math.floor(Math.random()*500);
     var y = Math.floor(Math.random()*500);
     object.style.top = x + 'px';
     object.style.left = y + 'px';
 };
+object.addEventListener('click', () => {
+    a.style.visibility = 'visible';
+});
